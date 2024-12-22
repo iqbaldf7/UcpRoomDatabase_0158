1a33413970b28ed66aea08a0a0549517db70dd9f
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-23 06:33:31 +0700

    menambah file dari matakuliah ke entity

--- app/src/main/java/com/example/ucp2_roomdatabase/data/entity/Matakuliah.kt
+++ app/src/main/java/com/example/ucp2_roomdatabase/data/entity/Matakuliah.kt
@@ -2,11 +2,13 @@ package com.example.ucp2_roomdatabase.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "matakuliah")
-data class Matakuliah(
+@Entity (tableName = "matakuliah")
+data class MataKuliah(
     @PrimaryKey
-    val id: Int = 0,
-    val kodeMatakuliah: String,
-    val namaMatakuliah: String,
-    val sks: Int
+    val kode : String,
+    val nama : String,
+    val sks : String,
+    val semester : String,
+    val jenis : String,
+    val dosenPengampu : String
 )
