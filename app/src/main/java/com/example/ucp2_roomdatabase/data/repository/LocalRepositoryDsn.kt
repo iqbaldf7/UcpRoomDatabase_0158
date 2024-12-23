a6b78ee256f95da1087c3c897e220f6bf42bdd41
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-23 10:41:34 +0700

    menambah import DosenDao dan Dosen entity untuk data di database

---
+++ app/src/main/java/com/example/ucp2_roomdatabase/data/repository/LocalRepositoryDsn.kt
@@ -0,0 +1,23 @@
+package com.example.ucp2_roomdatabase.data.repository
+import com.example.ucp2_roomdatabase.data.dao.DosenDao
+import com.example.ucp2_roomdatabase.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDsn(
+    private val dosenDao: DosenDao
+) : RepositoryDosen {
+
+
+    override suspend fun insertDosen(dosen: Dosen) {
+        dosenDao.insertDosen(dosen)
+    }
+
+
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+
+    override fun getDosen(nidn: String) : Flow<Dosen>{
+        return dosenDao.getDosen(nidn)
+    }
+}
