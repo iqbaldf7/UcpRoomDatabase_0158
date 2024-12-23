dc64bdb925a40d2d80fc665bcadfd6bc05f87412
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-23 10:16:00 +0700

    melakukan update file dan penyesuaian code MatakuliahDao

--- app/src/main/java/com/example/ucp2_roomdatabase/data/dao/MatakuliahDao.kt
+++ app/src/main/java/com/example/ucp2_roomdatabase/data/dao/MatakuliahDao.kt
@@ -1,13 +1,28 @@
 package com.example.ucp2_roomdatabase.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
-import com.example.ucp2_roomdatabase.data.entity.Matakuliah
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2_roomdatabase.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
 
-interface MatakuliahDao {
+@Dao
+interface MataKuliahDao {
     @Insert
-    suspend fun insertMstakuliah(matakuliah: Matakuliah)
+    suspend fun insertMataKuliah(matakuliah: MataKuliah)
 
+    @Query("SELECT * FROM matakuliah")
+    fun getAllMataKuliah(): Flow<List<MataKuliah>>
 
+    @Update
+    suspend fun updateMataKuliah(matakuliah: MataKuliah)
 
+    @Delete
+    suspend fun deleteMataKuliah(matakuliah: MataKuliah)
 
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getDetailMataKuliah(kode: String): Flow<MataKuliah>
 }
+
